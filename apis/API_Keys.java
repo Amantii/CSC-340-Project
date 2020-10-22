2f42edd88efb829189b84a0148bde981832dcd7c
Imran Al Nafiee <43937506+imranaliraqi@users.noreply.github.com>
2020-10-22 18:37:18 -0400

    Add files via upload

---
+++ apis/API_Keys.java
@@ -0,0 +1,16 @@
+package apis;
+
+/**
+ *
+ * @Author Imran Al NAfiee
+ *
+ * Updated October 22, 2020
+ */
+public class API_Keys {
+
+    public static String Appointment() {
+        String api_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhIjoiYWxsIiwibSI6IjM2MTg4IiwidCI6MTYwMDk4NzQwNCwiaWQiOjgzfQ.iHfIkU-wINYHzQLZyPTeOpw5zwwqEJXjqlk07oeshFo";
+
+        return api_key;
+    }
+}
