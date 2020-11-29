1bc8b33c56db30658970d08b1813b77a418958c6
Imran Al Nafiee <43937506+imranaliraqi@users.noreply.github.com>
2020-11-29 18:11:16 -0500

    updated codes

---
+++ apis/API_Keys.java
@@ -0,0 +1,25 @@
+package apis;
+
+/**
+ * Class used to store the API keys.
+ *
+ * @Author Imran Al Nafiee
+ *
+ * Updated October 29, 2020
+ */
+
+public class API_Keys {
+
+    public static String Appointment() {
+        String api_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhIjp7ImFwcCI6InJjZGUiLCJhdmwiOiJyIiwiY29uIjoicmNlZCIsInNlciI6InIiLCJ1c3IiOiJyIn0sIm0iOiIzNjE4OCIsInQiOjE2MDY1Mjk4NzksImlkIjo5MX0.xfA7WCQXpXVh2S--zgDsRhOj50EaCJv6EC2oUlfHz94";
+
+        return api_key;
+    }
+    
+    
+    public static String userId(){
+    String user_id = "usr_90zdaF1UYh3UjJzV0o3NxBTU4MXSXVVZ";
+
+        return user_id;
+    }
+}
